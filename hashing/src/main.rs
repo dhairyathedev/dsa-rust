400dad5afa991dfa693f069665ab35472a376212
Dhairya Shah <hello@dhairyashah.dev>
2025-02-22 16:16:58 +0530

    update

--- hashing/src/main.rs
+++ hashing/src/main.rs
@@ -1,20 +1,11 @@
-use std::io;
-use std::collections::HashMap;
+mod logic;
+use logic::main::{
+    basic_hash,
+    char_hashmap
+};
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let _n = input.trim().parse::<i32>().ok();
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let nums: Vec<i32> = input.split_whitespace().filter_map(|s| s.trim().parse::<i32>().ok()).collect();
-
-    // precompute
-    let mut hash: HashMap<i32, i32> = HashMap::new();
-    for &n in &nums {
-        *hash.entry(n).or_insert(0) += 1;
-    }
-    println!("{:?}", hash);
+    //basic_hash();
+    char_hashmap();
     println!("Hello, world!");
 }
